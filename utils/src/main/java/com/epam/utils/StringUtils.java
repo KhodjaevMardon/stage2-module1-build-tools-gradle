d88b14aadbc36e7a4d9d6bfbbebd357c40862e3f
Mardon Khodjaev <zombiee4407@gmail.com>
2023-01-28 14:26:24 +0500

    finished the task, i guess

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,15 @@
 package com.epam.utils;
 
+import java.math.BigDecimal;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        BigDecimal bigDecimal;
+        try {
+            bigDecimal = new BigDecimal(str);
+        } catch (RuntimeException e) {
+            return false;
+        }
+        return bigDecimal.abs().equals(bigDecimal) && bigDecimal.intValue() != 0;
     }
 }
